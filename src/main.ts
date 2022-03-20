f64533e06f32e8a43a48948ba568d95d8062a420
Rotfuchs von Vulpes <derfuchs.pa@gmail.com>
2022-03-19 22:26:02 -0400

    fullscreen

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 // @ts-ignore
 import GlslCanvas from 'glslCanvas';
 
-let canvas = document.getElementById('glslCanvas');
+let canvas = document.getElementById('glslCanvas') as HTMLCanvasElement;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 new GlslCanvas(canvas);
